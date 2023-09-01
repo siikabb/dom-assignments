7b0324d01e26bd6a2d361de723e6106b1db8bb5b
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-09-01 14:15:18 +0300

    excercise 1 moved to exc 2

--- t2/t2.js
+++ t2/t2.js
@@ -28,3 +28,22 @@ const todoList = [
 ];
 
 // add your code here
+const ul = document.querySelector('ul');
+
+for (const item of todoList) {
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = `todo-${item.id}`;
+  const label = document.createElement('label');
+  label.for = `todo-${item.id}`;
+  label.innerText = item.task;
+
+  if (item.completed) {
+    input.checked = true;
+  }
+  li.appendChild(input);
+  li.appendChild(label);
+
+  ul.appendChild(li);
+}
